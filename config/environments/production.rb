3850401fd908ca6d7cd1f889561030c53ce8521c
mk <mk@mkdeiMac.local>
2018-04-08 11:19:54 +0800

    fix bug for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,9 +22,11 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # 如果本地测试请用下面的, 否则asset文件不加载
+  # config.public_file_server.enabled = true 
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
